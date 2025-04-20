833feaabf6f87e7f67ada9c3fd66304f058d9b33
mohd-ravish <mohdravish2003@gmail.com>
2025-04-20 11:27:31 +0530

    add frontend url in server.js

--- Server/server.js
+++ Server/server.js
@@ -1,39 +1,42 @@
 import express from "express";
-const app=express()
-import { db } from "./connect.js"
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
-import commentRoutes from "./routes/comments.js"
-import cookieParser from "cookie-parser"    
-import cors from 'cors'
+const app = express();
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import cookieParser from "cookie-parser";    
+import cors from 'cors';
 import bodyParser from "body-parser";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-//middlewares
-app.use((req, res, next)=>{
+// CORS setup
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
-})
+});
 
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors({
-    origin: "http://localhost:3000",
-}))
-app.use(cookieParser())
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",  
+    credentials: true
+}));
 
+app.use(cookieParser());
 
-app.get("/", (req, res)=>{
-    res.send("Hello from backend")
-})
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello from backend");
+});
 
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/comments", commentRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
-app.listen(8800, ()=>{
-    console.log("SERVER IS RUNNING ON PORT 8800")
-})
+// Server
+app.listen(8800, () => {
+    console.log("SERVER IS RUNNING ON PORT 8800");
+});
